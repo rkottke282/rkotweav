a5960e1eb976631db3a58cd3f5d1758fa6f924e8
Reed Kottke <reed>
2024-01-23 20:05:03 -0500

    feat: implement next.js, typescript and react

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "R. Kot-Weav",
+  description: "An app without a purpose"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
