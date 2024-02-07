15b189f741d4b48de953fc6db9066580494cd36b
Reed Kottke <reed>
2024-02-06 19:28:43 -0500

    fix: formatting of apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Github{" "}
+            Github
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            A bunch of stuff I've made
+            {`A bunch of stuff I've made`}
           </p>
         </a>
 
