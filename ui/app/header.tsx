2dbee71ea751ced6aa89daf6d11ff86677756f71
Reed Kottke-Weaver <rkotweav@Reeds-MacBook-Air.local>
2024-10-19 21:00:30 -0400

    fix: dark mode

--- ui/app/header.tsx
+++ ui/app/header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-green-100">
+    <header className="bg-green-100 dark:bg-gray-600">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
