bbf914fed190e1f99fa612c56213e84b8a6fe001
Reed Kottke-Weaver <rkotweav@Reeds-MacBook-Air.local>
2024-12-15 14:42:35 -0500

    feat: authorizing and passing email

--- api/shared/authorizer/src/index.ts
+++ api/shared/authorizer/src/index.ts
@@ -1,16 +1,31 @@
 import { APIGatewayEventRequestContextLambdaAuthorizer, APIGatewayEventRequestContextV2, APIGatewayRequestAuthorizerEventV2, APIGatewayRequestSimpleAuthorizerHandlerV2WithContext, APIGatewaySimpleAuthorizerWithContextResult, Callback } from 'aws-lambda'
+import { TokenInfo } from 'google-auth-library';
+import { getTokenInfo } from './google-auth';
+
+const CLIENT_ID: String = '484680450142-kcatfskg0bfr84cvgkjg3933v4pu96qn.apps.googleusercontent.com';
 
 const handler = async (event: APIGatewayRequestAuthorizerEventV2, 
     context: APIGatewayEventRequestContextV2) => {
-    console.log('event: ', JSON.stringify(event));
-    const response = {
-        isAuthorized: true,
-        context: {
-            identitySource: event.identitySource
+        try {
+            const identitySource: string = event.identitySource[0];
+            if (!identitySource) throw Error('Bad authorization');
+            const token: string = identitySource.replace('Bearer ', '');
+            const tokenInfo: TokenInfo = await getTokenInfo(token);
+            if (!tokenInfo) throw Error('Failed to retrieve token info');
+            if (tokenInfo.aud != CLIENT_ID) throw Error('Incorrect client id');
+            return {
+                isAuthorized: true,
+                context: {
+                    email: tokenInfo.email
+                }
+            }
+        } catch (err) {
+            console.log('Failed authorizer.  Error: ', err);
+            return {
+                isAuthorized: false,
+            }
         }
-    }
-    console.log('response: ', JSON.stringify(response));
-    return response;
+    
 }
 
 export { handler };
